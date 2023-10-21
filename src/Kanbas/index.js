45fd2a2c2a2968cf88c4777b84516b62452982e1
ibamirah <amirahibrahim22@gmail.com>
2023-10-20 20:26:58 -0400

    assignment 3

---
+++ src/Kanbas/index.js
@@ -0,0 +1,26 @@
+import KanbasNavigation from "./KanbasNavigation";
+import Dashboard from "./Dashboard";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Courses from "./Courses";
+import {Link} from "react-router-dom";
+function Kanbas() {
+  return (
+    <div className="d-flex">
+      <KanbasNavigation />
+      <div>
+        <h1>Kanbas Navigation</h1>
+      </div>
+      <div>
+      <Routes>
+          <Route path="/" element={<Navigate to="Dashboard" />} />
+          <Route path="Account" element={<h1>Account</h1>} />
+          <Route path="Dashboard" element={<Dashboard />} />
+          <Route path="Courses/:courseId/*" element={<Courses />} />
+        </Routes>
+
+      </div>
+    </div>
+  );
+}
+export default Kanbas;
+
